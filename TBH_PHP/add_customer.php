b0dd78716b9d88a9bb289c968dd5981c971bcb8b
benghong <1211206859@student.mmu.edu.my>
2023-06-17 20:14:00 +0800

    update

--- TBH_PHP/add_customer.php
+++ TBH_PHP/add_customer.php
@@ -14,15 +14,15 @@
 			<p>Customer ID: <input type="text" name="customer_id" >
 			<p>Customer Name: <input type="text" name="customer_name">
 			<p>Customer Email: <input type="text" name="customer_email">
-			<p>Customer Phone Number: <input type="text" name="customer_phonenumber">
-            <p>Customer Registeration Date: <input type="date" name="customer_registerdate">
-		    <p>Customer Password: <input type="textbox" name="customer_password">
+			<p>Customer Phone Number: <input type="text" name="customer_phone_number">
+            <p>Customer Registeration Date: <input type="date" name="customer_register_date">
+		    <p>Customer Password: <input type="text" name="customer_password">
 						
 			<p><input type="submit" name="savebtn" value="Save New Customer">
 
 		</form>
 
-		<input type="button" value="view customer list" onclick="location='customer_list.php'">
+		<input type="button" value="View Customer List" onclick="location='customer_list.php'">
 		<input type="button" value="View Booking List" onclick="location='bookinglist.php'">
 </body>
 </html>
@@ -34,8 +34,8 @@ if (isset($_POST["savebtn"]))
 	$cid = $_POST["customer_id"];
 	$cname= $_POST["customer_name"];
 	$cemail=$_POST["customer_email"];
-	$cphonenumber=$_POST["customer_phonenumber"];
-    $cregisterdate=$_POST["customer_registerdate"];
+	$cphonenumber=$_POST["customer_phone_number"];
+    $cregisterdate=$_POST["customer_register_date"];
     $cpassword=$_POST["customer_password"];
 	
 	
@@ -53,7 +53,7 @@ if (isset($_POST["savebtn"]))
 	else
 	{
 	   //else insert into database
-		$success=mysqli_query($connect,"INSERT INTO customer (customer_id, customer_name, customer_email, customer_phonenumber, customer_registerdate, customer_password )
+		$success=mysqli_query($connect,"INSERT INTO customer(customer_id,customer_name,customer_email,customer_phone_number,customer_register_date,customer_password)
 		VALUES ('$cid','$cname','$cemail','$cphonenumber','$cregisterdate','$cpassword')");
 		
 		if($success){
---
+++ TBH_PHP/customer_list.php
@@ -0,0 +1,65 @@
+<?php include("dataconnection.php"); ?>
+
+<html>
+<head></head>
+
+<script type="text/javascript">
+//delete confirmation
+function confirmation()
+{
+	var option;
+	option=confirm("Are you sure you want to delete this customer details?");
+	return option;
+}
+</script>
+
+<body>
+    <!--list of room type-->
+    <h1>List of Customer Details </h1>
+    <table border="1" width="650px">
+			<tr>
+                <th>Customer ID</th>
+				<th>Customer Name</th>
+				<th>Customer Email</th>
+				<th>Customer Phone Number</th>
+                <th>Customer Registeration Date</th>	
+                <th>Customer Password</th>					
+				<th colspan="3">Actions</th>
+			</tr>
+
+            <?php
+				$result=mysqli_query($connect,"SELECT * FROM customer WHERE customer_isDelete=0");
+				while($row=mysqli_fetch_assoc($result)){
+			?>
+					<tr>
+						<td><?php echo $row["customer_id"]?></td>
+						<td><?php echo $row["customer_name"]?></td>
+						<td><?php echo $row["customer_email"]?></td>
+                        <td><?php echo $row["customer_phone_number"]?></td>
+                        <td><?php echo $row["customer_registe_date"]?></td>
+                        <td><?php echo $row["customer_password"]?></td>
+						
+						<td><a href="edit.php?edit&code=<?php echo $row["customer_id"];?>">Edit</a></td>
+						<td><a href="customer_list.php?del&code=<?php echo $row ["customer_id"];?>" onclick="return confirmation();">Delete</a></td>
+			<?php
+				}	
+			?>
+		</table>
+</body>
+</html>
+
+<?php
+    //remove room from room list
+    if (isset($_GET["del"])) 
+    {
+        $code=$_GET["code"];
+        //update room table and set room_isdelete to 1
+        $sql=mysqli_query($connect,"UPDATE customer SET customer_isdelete=1 WHERE customer_id='$cid'")
+        ?>
+        <script>
+            alert("Record Deleted.")
+        </script>
+        <?php
+        header("refresh:0.5; url=customer_list.php");
+    }
+?>
